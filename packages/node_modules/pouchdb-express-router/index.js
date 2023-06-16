24fcd432e40b9fa76bf9af20470293b0815b40a3
Frank Lemanschik <frank@lemanschik.com>
2023-06-16 11:44:16 +0000

    16. Juni 2023, 13:44

---
+++ packages/node_modules/pouchdb-express-router/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var express = require('express');
+
+var routes = {
+  'db': require('./lib/routes/db.js'),
+  'document': require('./lib/routes/document.js'),
+  'attachments': require('./lib/routes/attachments.js')
+};
+
+module.exports = function(PouchDB) {
+
+  var app = express.Router();
+
+  app.use(function (req, res, next) {
+    for (var prop in req.query) {
+      try {
+        req.query[prop] = JSON.parse(req.query[prop]);
+      } catch (e) {}
+    }
+    next();
+  });
+
+  app.get('/', function (req, res) {
+    res.status(200).send({'pouchdb-express-router': 'Welcome!'});
+  });
+
+  app.get('/_session', function (req, res) {
+    res.status(200).send({
+      'ok': true,
+      'userCtx':{"name":null,"roles":["_admin"]}
+    });
+  });
+
+  routes.db(app, PouchDB);
+  routes.attachments(app, PouchDB);
+  routes.document(app, PouchDB);
+
+  app.use(function (req, res) {
+    res.status(404).send( {
+      error: "not_found",
+      reason: "missing"
+    });
+  });
+
+  return app;
+};
+
+// Used for testing porpoises
+// var PouchDB = require('pouchdb');
+// var app = express();
+// app.use(module.exports(PouchDB));
+// app.listen(5985);
